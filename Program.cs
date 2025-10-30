7ec3877aa6c6c546ffe53a4830403bacb157fbe0
Xssama <ouhsaini2005@gmail.com>
2025-10-30 14:53:49 +0100

    implicitly typed variables

--- Program.cs
+++ Program.cs
@@ -5,11 +5,12 @@ namespace Main
     {
         static void Main(string[] args) 
             {
-            string MyName = "Oussama Hsaini";
-            string language = "C#";
+            var MyName = "Oussama Hsaini";
+            var language = "C#";
                Console.WriteLine("My name is {0}!", MyName);
                Console.WriteLine("I'm learning {0}", language);
-            int X = 10, Y = 20;
+            var X = 10;
+            var Y = 20;
                Console.Write("The sum of {0} + {1} is: {2}", X, Y, (X+Y));
                
                Console.ReadKey();
