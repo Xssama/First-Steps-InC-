d9a87fe257ab63428a9ef8009c7b8d872528dfc1
Xssama <ouhsaini2005@gmail.com>
2025-10-30 13:27:42 +0100

    Second commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Main
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+            {
+               Console.WriteLine("My name is oussama!");
+               Console.WriteLine("I'm learning c#");
+               Console.Write("The sum of 10 + 20 is: ");
+               Console.Write(10 + 20);
+               Console.ReadKey();
+            }
+    }
+}
