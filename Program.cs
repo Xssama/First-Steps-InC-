0692e5d95ee82218779eb58d0ca13dc5735e9ce9
Xssama <ouhsaini2005@gmail.com>
2025-10-31 17:16:54 +0100

    Anonymous Type

--- Program.cs
+++ Program.cs
@@ -7,13 +7,20 @@ namespace Main
         static void Main(string[] args)
         {
 
-            //  Nullable<int> can be assigned any value
-            //  from -2147483648 to 2147483647, or a null value.
-
-            Nullable<int> i = null;
-
-
-            Console.WriteLine(i);
+            //you dont specify any type here , automatically will be specified
+            var Var1 = new { Firstname = "nassim", Lastname = "Zaml", Age = 20 };
+           
+            Console.WriteLine("Exemple1:");
+            Console.WriteLine("First Name: " + Var1.Firstname);
+            Console.WriteLine("Last Name : " + Var1.Lastname);
+            Console.WriteLine("Age: " + Var1.Age);
+            var Var2 = new { Firstname = "Marwane", Lastname = "Rabhi", Age = 23, adresse = new { city = "Oujda", Country = "Morocco" } };
+            Console.WriteLine("\n\nExemple2");
+            Console.WriteLine("first Name: "+ Var2.Firstname);
+            Console.WriteLine("Last Name: "+ Var2.Lastname);
+            Console.WriteLine("Age: "+ Var2.Age);
+            Console.WriteLine("City: "+ Var2.adresse.city);
+            Console.WriteLine("Cuntry: "+ Var2.adresse.Country);
             Console.ReadKey();
 
         }
