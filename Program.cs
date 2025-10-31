79134b6f3eede10ad13ed2519abf9a5907525278
Xssama <ouhsaini2005@gmail.com>
2025-10-31 17:26:22 +0100

    struct

--- Program.cs
+++ Program.cs
@@ -4,23 +4,39 @@ namespace Main
 {
     internal class Program
     {
+
+
+        struct stStudent
+        {
+            public string FirstName;
+            public string LastName;
+        }
+
         static void Main(string[] args)
         {
 
-            //you dont specify any type here , automatically will be specified
-            var Var1 = new { Firstname = "nassim", Lastname = "Zaml", Age = 20 };
-           
-            Console.WriteLine("Exemple1:");
-            Console.WriteLine("First Name: " + Var1.Firstname);
-            Console.WriteLine("Last Name : " + Var1.Lastname);
-            Console.WriteLine("Age: " + Var1.Age);
-            var Var2 = new { Firstname = "Marwane", Lastname = "Rabhi", Age = 23, adresse = new { city = "Oujda", Country = "Morocco" } };
-            Console.WriteLine("\n\nExemple2");
-            Console.WriteLine("first Name: "+ Var2.Firstname);
-            Console.WriteLine("Last Name: "+ Var2.Lastname);
-            Console.WriteLine("Age: "+ Var2.Age);
-            Console.WriteLine("City: "+ Var2.adresse.city);
-            Console.WriteLine("Cuntry: "+ Var2.adresse.Country);
+            //A struct object can be created with or without the new operator,
+            //same as primitive type variables.
+
+            stStudent Student;
+            stStudent Student2 = new stStudent();
+
+
+            Student.FirstName = "Mohammed";
+            Student.LastName = "Abu-Hadhoud";
+
+
+            Console.WriteLine(Student.FirstName);
+            Console.WriteLine(Student.LastName);
+
+
+            Student2.FirstName = "Ali";
+            Student2.LastName = "Ahmed";
+
+
+            Console.WriteLine(Student2.FirstName);
+            Console.WriteLine(Student2.LastName);
+
             Console.ReadKey();
 
         }
