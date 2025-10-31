9cb916accdca4b45ae2ea29aaebb3f7b5585377b
Xssama <ouhsaini2005@gmail.com>
2025-10-31 19:33:19 +0100

    Operators

--- Program.cs
+++ Program.cs
@@ -8,12 +8,18 @@ namespace Main
         static void Main(string[] args)
         {
 
-            DateTime dt1 = new DateTime(2025, 10, 31);
-            DateTime dt2 = new DateTime(2024, 10, 31);
-            // Hours, Minutes, Seconds
-            TimeSpan ts = dt1.Subtract(dt2);
+            DateTime dt1 = new DateTime(2015, 12, 20);
+            DateTime dt2 = new DateTime(2016, 12, 31, 5, 10, 20);
+            TimeSpan time = new TimeSpan(10, 5, 25, 50);
 
-            Console.WriteLine(ts.TotalDays);
+            Console.WriteLine(dt2 + time); // 1/10/2017 10:36:10 AM
+            Console.WriteLine(dt2 - dt1); //377.05:10:20
+            Console.WriteLine(dt1 == dt2); //False
+            Console.WriteLine(dt1 != dt2); //True
+            Console.WriteLine(dt1 > dt2); //False
+            Console.WriteLine(dt1 < dt2); //True
+            Console.WriteLine(dt1 >= dt2); //False
+            Console.WriteLine(dt1 <= dt2); //True
             Console.ReadKey();
 
         }
